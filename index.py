225fbad4bbe19ba58098e7038e2f32af3c38e253
yuya takei <takewell.dev@gmail.com>
2019-07-22 10:55:09 +0900

    Add

--- index.py
+++ index.py
@@ -2,12 +2,11 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import scipy as sp
 
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))
 plt.show()
 
-#%%
-print("hello")
 
 #%%
