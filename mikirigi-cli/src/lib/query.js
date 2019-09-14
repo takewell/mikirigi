be37430634f39562a7a40d4606da2111fcfa00cf
yuya takei <takewell.dev@gmail.com>
2019-09-14 20:31:54 +0900

    Add new query

--- mikirigi-cli/src/lib/query.js
+++ mikirigi-cli/src/lib/query.js
@@ -26,3 +26,77 @@ exports.GetRepositoryStars = gql`
     }
   }
 `;
+
+exports.GetRepositoryLanguages = gql`
+  query GetRepositoryLanguages(
+    $repoName: String!
+    $ownerName: String!
+    $corsor: String
+  ) {
+    repository(name: $repoName, owner: $ownerName) {
+      name
+      languages(
+        first: 100
+        after: $corsor
+        orderBy: { field: SIZE, direction: DESC }
+      ) {
+        totalCount
+        pageInfo {
+          hasNextPage
+          startCursor
+          endCursor
+        }
+        totalSize
+        edges {
+          node {
+            name
+          }
+          size
+        }
+      }
+    }
+  }
+`;
+
+exports.GetRepositoryRelease = gql`
+  query GetRepositoryRelease(
+    $repoName: String!
+    $ownerName: String!
+    $corsor: String
+  ) {
+    repository(name: $repoName, owner: $ownerName) {
+      name
+      releases(
+        first: 100
+        after: $corsor
+        orderBy: { field: CREATED_AT, direction: ASC }
+      ) {
+        totalCount
+        pageInfo {
+          hasNextPage
+          startCursor
+          endCursor
+        }
+        edges {
+          node {
+            releaseAssets(first: 100) {
+              totalCount
+              nodes {
+                name
+                contentType
+                size
+              }
+            }
+            name
+            createdAt
+            publishedAt
+            updatedAt
+            isDraft
+            isPrerelease
+            tagName
+          }
+        }
+      }
+    }
+  }
+`;
