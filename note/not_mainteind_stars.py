bd7ac1bfa7fafc137875b470e0da148b271bf53d
yuya takei <takewell.dev@gmail.com>
2019-10-10 18:15:54 +0900

    Add

---
+++ note/not_mainteind_stars.py
@@ -0,0 +1,109 @@
+
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import scipy as sp
+import pandas as pd
+import json
+import datetime
+
+#%%
+
+# TODO: 
+# - refactor class
+# - snake case
+
+def convertDataSiries(name):
+  name = name.split('/')
+  fname = 'data/stars/' + name[0] +'_' + name[1] + '_stars.json'
+  repository = pd.read_json(fname)
+  return pd.Series(pd.to_datetime(repository['staredAts'])).dt.date
+
+def github_life_series():
+  github_life = np.arange('2008-01-01', datetime.date.today(), dtype='datetime64[D]')
+  return pd.Series(github_life, name='t').dt.date
+
+def create_star_df(name):
+  github_life = github_life_series()
+  repository = convertDataSiries(name)
+  star_counts = []
+  for d in github_life:
+    count = repository[repository == d].count()
+    star_counts.append(count)
+
+  return pd.DataFrame(star_counts, columns=['star'])
+  
+
+def plotStars(name):
+  star_df = create_star_df(name)
+  github_life = github_life_series()
+  result = pd.concat([star_df, github_life], axis=1)
+  result.plot(x='t', y='star', ylim=[0, 100])
+
+def get_x_index(name):
+  result = create_star_df(name)
+  arr = result['star'].values.tolist()
+  return h_index(arr)
+
+def h_index(citations):
+  n = len(citations)
+  equal_h = [0] * (n+1)
+  for h in range(n):
+      if citations[h] >= n: equal_h[n] += 1
+      else: equal_h[citations[h]] += 1
+  
+  s = 0
+  for h in range(n,0, -1):
+      s += equal_h[h]
+      if s>=h:
+          return h
+  return 0
+
+
+#%%
+f = open('data/assets/not maintained_repositorys.json')
+repos = json.load(f)
+
+for name in repos['not_maintained_repository']:
+  print(name)
+  print(get_x_index(name))
+f.close()
+
+#%%
+plotStars('allmarkedup/purl')
+
+
+#%%
+
+# not_maintained なレポジトリを集結させた
+f = open('data/assets/not maintained_repositorys.json')
+repos = json.load(f)
+
+for name in repos['not_maintained_repository']:
+  plotStars(name)
+
+f.close()
+
+#%%
+
+f = open('data/assets/javascript_engine_repositorys.json')
+repos = json.load(f)
+
+for name in repos['javascript_engine_repositorys']:
+  plotStars(name)
+
+f.close()
+
+
+#%%
+# v8/v8
+# elastic/elasticsearch
+# arduino/Arduino
+# bitcoin/bitcoin
+
+
+plotStars('bitcoin/bitcoin')
+
+
+#%%
